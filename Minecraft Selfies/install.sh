0f9283024c62de77382c5200687eca850aa098e2
Ahsen Baig <146462+AhsenBaig@users.noreply.github.com>
2022-11-04 16:50:31 -0700

    Update install.sh

--- Minecraft Selfies/install.sh
+++ Minecraft Selfies/install.sh
@@ -4,4 +4,12 @@ sudo ap-get update && apt-get upgrade && apt-get install -y python3-skimage
 
 pip install mcpi
 
-sudo apt-get install python-dev && sudo pip install Pillow
+sudo apt install python3-minecraftpi
+
+# sudo apt-get install python-dev && sudo pip install Pillow
+
+
+# minecraft-pi-reborn
+# https://github.com/MCPI-Revival/minecraft-pi-reborn/blob/master/docs/INSTALL.md
+sudo apt install libgtk-3-0
+sudo apt install libopenal1
